f087a36d29f64b1db6b497a9f52bf816c96bd723
PoweredByPie <poweredbypie@users.noreply.github.com>
2023-02-18 19:31:26 -0800

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Small and simple status bar";
+
+  inputs.nixpkgs.url = "nixpkgs";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in
+  {
+    defaultPackage."${system}" = pkgs.stdenv.mkDerivation rec {
+      pname = "statxt";
+      version = "0.1.0";
+
+      src = ./.;
+
+      nativeBuildInputs = [ pkgs.zig ];
+
+      dontConfigure = true;
+
+      preBuild = ''
+        export HOME=$TMPDIR
+      '';
+
+      installPhase = ''
+        runHook preInstall
+        zig build -Drelease-small -Dcpu=baseline --prefix $out install
+        runHook postInstall
+      '';
+    };
+  };
+}
